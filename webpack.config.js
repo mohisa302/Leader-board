dead1d9cb2fac80e55b6a5bf59f854105232810f
mohi <mohisadeghi302@gmail.com>
2023-01-31 01:43:26 +0330

    Add style|Html

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,6 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {
